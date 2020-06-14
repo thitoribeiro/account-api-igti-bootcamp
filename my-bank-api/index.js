38e1d2865fc571382112d0189c819d2caf44678a
thitoribeiro <thito.ribeiro@hotmail.com>
2020-06-13 22:51:04 -0300

    bank api

---
+++ my-bank-api/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var fs = require("fs");
+var app = express();
+var accountsRouter = require("./routes/accounts.js");
+
+global.fileName = "accounts.json";
+
+app.use(express.json());
+app.use("/account", accountsRouter);
+
+app.listen(3000, function () {
+  try {
+    fs.readFile(global.fileName, "utf8", (err, data) => {
+      if (err) {
+        const initialJson = {
+          nextId: 1,
+          accounts: [],
+        };
+        fs.writeFile(global.fileName, JSON.stringify(initialJson), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  console.log("API started!");
+});
